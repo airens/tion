75b0373ff6dbf69be64c9cb018e9cc830ea201f6
airens <airens@mail.ru>
2019-11-16 13:18:41 +0300

    Upgraded to version 0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from os.path import join, dirname
 
 setup(name='tion',
-      version='0.3',
+      version='0.4',
       description='Tion Magic Air API',
       url='http://github.com/airens/tion',
       author='Valeriy Chistyakov',
