32e6a438b87a077b1231959af43b44593a2c67be
airens <airens@mail.ru>
2019-12-02 22:58:06 +0300

    0.62

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from os.path import join, dirname
 
 setup(name='tion',
-      version='0.61',
+      version='0.62',
       description='Tion Magic Air API',
       url='http://github.com/airens/tion',
       author='Valeriy Chistyakov',
