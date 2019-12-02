409ea4be02533ec32e048bb681a7d902b7594856
airens <airens@mail.ru>
2019-12-02 14:23:42 +0300

    completely new tests to match version 0.5

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,19 +4,40 @@ from tion import TionApi, Breezer, Zone, MagicAir
 
 
 @pytest.fixture
+def api_no_saved_auth() -> TionApi:
+    email, password = os.environ.get("TION_AUTH").split(',')
+    return TionApi(email, password, save_auth=False)
+
+
+@pytest.fixture
 def api() -> TionApi:
     email, password = os.environ.get("TION_AUTH").split(',')
     return TionApi(email, password)
 
 
 @pytest.fixture
-def zone(api: TionApi) -> Breezer:
+def zone(api: TionApi) -> Zone:
     return api.get_zones(name_part="гостиная")[0]
-
 
 @pytest.fixture
 def breezer(api: TionApi) -> Breezer:
     return api.get_devices(name_part="breezer 3s")[0]
+
+
+@pytest.fixture
+def breezer_manual(breezer: Breezer) -> Breezer:
+    breezer.zone.mode = "manual"
+    breezer.zone.send()
+    assert breezer.zone.mode == "manual", "Couldn't set breezer zone mode to manual!"
+    return breezer
+
+
+@pytest.fixture
+def breezer_auto(breezer: Breezer) -> Breezer:
+    breezer.zone.mode = "auto"
+    breezer.zone.send()
+    assert breezer.zone.mode == "auto", "Couldn't set breezer zone mode to auto!"
+    return breezer
 
 
 @pytest.fixture
--- tests/test_tion.py
+++ tests/test_tion.py
@@ -3,102 +3,155 @@ import copy
 from time import sleep
 
 
+zone_prev: Zone = None
+breezer_prev: Breezer = None
+
+
+TESTS_DELAY = 1
+
+
+def test_api_init_no_saved_auth(api_no_saved_auth: TionApi):
+    assert api_no_saved_auth.authorization, "Authorisation failed!"
+    assert api_no_saved_auth._data, "No data got!!"
+    sleep(TESTS_DELAY)  # let the server "rest"
+
+
 def test_api_init(api: TionApi):
     assert api.authorization, "Authorisation failed!"
     assert api._data, "No data got!!"
+    sleep(TESTS_DELAY)  # let the server "rest"
 
 
-# def test_api_getting_data_interval(api: TionApi):
-#     api.get_data()
-#     time1 = api._data.connection.last_packet_time
-#     api.get_data()
-#     time2 = api._data.connection.last_packet_time
-#     assert time1 == time2, "Api must update data no more than once per second!"
-#     sleep(2)
-#     api.get_data()
-#     time3 = api._data.connection.last_packet_time
-#     assert time3 > time2, "Api must update data once per second!"
+def test_tion_api_need_new_auth(api: TionApi):
+    api.authorization = ""
+    assert api.get_data(force=True) is True, "New auth should've received!"
+    sleep(TESTS_DELAY)  # let the server "rest"
+
+
+def test_tion_api_wait_for_task_returns_false_on_timeout_0_6(api: TionApi):
+    assert api.wait_for_task("asdf", max_time=0.6) is False, "Must return False on timeout!"
+    sleep(TESTS_DELAY)  # let the server "rest"
+
+
+def test_tion_api_wait_for_task_returns_false_on_bad_task_id(api: TionApi):
+    assert api.wait_for_task("asdf") is False, "Must return False on lack of bad task_id!"
+    sleep(TESTS_DELAY)  # let the server "rest"
 
 
 def test_zone_init(zone: Zone):
+    global zone_prev
+    zone_prev = copy.deepcopy(zone)  # save previous zone state
     assert zone.valid, "Data not loaded properly!"
+    sleep(TESTS_DELAY)  # let the server "rest"
 
 
-def test_zone_send(zone: Zone):
-    zone_prev = copy.deepcopy(zone)
-    zone.mode = "auto" if zone.mode == "manual" else "manual"
-    zone.target_co2 = 900 if zone.target_co2 != 900 else 901
+def test_zone_load(zone: Zone):
+    assert zone.load() is True, "Failed to load zone data!"
+    sleep(TESTS_DELAY)  # let the server "rest"
+
+
+def test_zone_not_send_if_not_valid(zone: Zone):
+    zone.target_co2 = None
+    assert zone.send() is False, "Should'nt have sent not valid zone!"
+    sleep(TESTS_DELAY)  # let the server "rest"
+
+
+def test_zone_mode_send(zone: Zone):
+    new_mode = "auto" if zone.mode == "manual" else "manual"
+    zone.mode = new_mode
     zone.send()
-    sleep(3)  # wait for update
-    zone.load()
-    assert zone.mode != zone_prev.mode, "Failed to set zone mode!"
-    assert zone.mode != zone_prev.target_co2, "Failed to set zone mode!"
-    zone_prev.send()  # return to prev values
-    sleep(3)
-    zone.load()
-    assert zone_prev.mode == zone.mode, "Failed to return mode to previous value!"
-    assert zone_prev.target_co2 == zone.target_co2, "Failed to return co2 to previous value!"
+    assert zone.mode == new_mode, "Failed to set zone mode!"
+    sleep(TESTS_DELAY)  # let the server "rest"
+
+
+def test_zone_co2_send(zone: Zone):
+    new_co2 = 900 if zone.target_co2 != 900 else 901
+    zone.target_co2 = new_co2
+    zone.send()
+    assert zone.target_co2 == new_co2, "Failed to set target co2!"
+    sleep(TESTS_DELAY)  # let the server "rest"
 
 
 def test_breezer_init(breezer: Breezer):
+    global breezer_prev
+    breezer_prev = copy.deepcopy(breezer)  # save previous zone state
     assert breezer.valid, "Data not loaded properly!"
+    sleep(TESTS_DELAY)  # let the server "rest"
 
 
-def test_breezer_manual_send(breezer: Breezer, zone: Zone):
-    zone_mode_prev = zone.mode
-    if zone.mode != "manual":
-        zone.mode = "manual"
-        zone.send()
-        sleep(3)
-    breezer_prev = copy.deepcopy(breezer)
-    breezer.is_on = True if not breezer.is_on else False
-    breezer.heater_enabled = True if not breezer.heater_enabled else False
-    breezer.t_set = 15 if breezer.t_set != 15 else 14
-    breezer.speed = 1 if breezer.speed != 1 else 2
-    breezer.send()
-    sleep(3)  # wait for update
-    breezer.load()
-    assert breezer.is_on != breezer_prev.is_on, "Failed to set breezer is_on!"
-    assert breezer.heater_enabled != breezer_prev.heater_enabled, "Failed to set breezer heater_enabled!"
-    assert breezer.t_set != breezer_prev.t_set, "Failed to set breezer t_set!"
-    assert breezer.speed != breezer_prev.speed, "Failed to set breezer speed!"
-    breezer_prev.send()  # return to prev values
-    sleep(3)
-    breezer.load()
-    assert breezer.is_on == breezer_prev.is_on, "Failed to set breezer is_on!"
-    assert breezer.heater_enabled == breezer_prev.heater_enabled, "Failed to set breezer heater_enabled!"
-    assert breezer.t_set == breezer_prev.t_set, "Failed to set breezer t_set!"
-    assert breezer.speed == breezer_prev.speed, "Failed to set breezer speed!"
-    if zone.mode != zone_mode_prev:
-        zone.mode = zone_mode_prev
-        zone.send()
-        sleep(3)
+def test_breezer_not_send_if_not_valid(breezer: Breezer):
+    breezer.speed = None
+    assert breezer.send() is False, "Should'nt have sent not valid breezer!"
+    sleep(TESTS_DELAY)  # let the server "rest"
 
 
-def test_breezer_auto_send(breezer: Breezer, zone: Zone):
-    zone_mode_prev = zone.mode
-    if zone.mode != "auto":
-        zone.mode = "auto"
-        zone.send()
-        sleep(3)
-    breezer_prev = copy.deepcopy(breezer)
-    breezer.speed_max_set = 6 if breezer.speed_max_set != 6 else 5
-    breezer.speed_min_set = 0 if breezer.speed_min_set != 0 else 1
-    breezer.send()
-    sleep(3)  # wait for update
-    breezer.load()
-    assert breezer.speed_max_set != breezer_prev.speed_max_set, "Failed to set breezer speed_max_set!"
-    assert breezer.speed_min_set != breezer_prev.speed_min_set, "Failed to set breezer speed_min_set!"
-    breezer_prev.send()  # return to prev values
-    sleep(3)
-    breezer.load()
-    assert breezer.speed_max_set == breezer_prev.speed_max_set, "Failed to set breezer speed_max_set!"
-    assert breezer.speed_min_set == breezer_prev.speed_min_set, "Failed to set breezer speed_min_set!"
-    if zone.mode != zone_mode_prev:
-        zone.mode = zone_mode_prev
-        zone.send()
-        sleep(3)
+def test_breezer_load(breezer: Breezer):
+    assert breezer.load() is True, "Failed to load breezer data!"
+    sleep(TESTS_DELAY)  # let the server "rest"
+
+
+def test_breezer_manual_send_is_on(breezer_manual: Breezer):
+    new_is_on = True if not breezer_manual.is_on else False
+    breezer_manual.is_on = new_is_on
+    breezer_manual.send()
+    assert breezer_manual.is_on == new_is_on, "Couldn't set is on!"
+    sleep(TESTS_DELAY)  # let the server "rest"
+
+
+def test_breezer_manual_send_heater_enabled(breezer_manual: Breezer):
+    new_heater_enabled = True if not breezer_manual.heater_enabled else False
+    breezer_manual.heater_enabled = new_heater_enabled
+    breezer_manual.send()
+    sleep(TESTS_DELAY)  # let the server "rest"
+
+
+def test_breezer_manual_send_t_set(breezer_manual: Breezer):
+    new_t_set = 15 if breezer_manual.t_set != 15 else 14
+    breezer_manual.t_set = new_t_set
+    assert breezer_manual.t_set == new_t_set, "Failed to set breezer t_set!"
+    sleep(TESTS_DELAY)  # let the server "rest"
+
+
+def test_breezer_manual_send_speed(breezer_manual: Breezer):
+    new_speed = 1 if breezer_manual.speed != 1 else 2
+    breezer_manual.speed = new_speed
+    breezer_manual.send()
+    assert breezer_manual.speed == new_speed, "Failed to set breezer speed!"
+    sleep(TESTS_DELAY)  # let the server "rest"
+
+
+def test_breezer_auto_send_speed_max_set(breezer_auto: Breezer):
+    new_speed_max_set = breezer_auto.speed_limit if breezer_auto.speed_max_set != breezer_auto.speed_limit \
+        else breezer_auto.speed_limit - 1
+    breezer_auto.speed_max_set = new_speed_max_set
+    breezer_auto.send()
+    assert breezer_auto.speed_max_set == new_speed_max_set, "Failed to set breezer speed_max_set!"
+    sleep(TESTS_DELAY)  # let the server "rest"
+
+
+def test_breezer_auto_send_speed_min_set(breezer_auto: Breezer):
+    new_speed_min_set = 1 if breezer_auto.speed_min_set != 1 else 2
+    breezer_auto.speed_min_set = new_speed_min_set
+    breezer_auto.send()
+    assert breezer_auto.speed_min_set == new_speed_min_set, "Failed to set breezer speed_min_set!"
+    sleep(TESTS_DELAY)  # let the server "rest"
 
 
 def test_magicair_init(magicair: MagicAir):
     assert magicair.valid, "Data not loaded properly!"
+    sleep(TESTS_DELAY)  # let the server "rest"
+
+
+def test_magicair_load(magicair: MagicAir):
+    assert magicair.load()
+    sleep(TESTS_DELAY)  # let the server "rest"
+
+
+def test_restore_breezer():
+    assert breezer_prev.send(), "Failed to send previous breezer parameters!"  # return to prev values
+    sleep(TESTS_DELAY)  # let the server "rest"
+
+
+def test_restore_zone():
+    assert zone_prev.send(), "Failed to send previous zone parameters!"  # return to prev values
+    sleep(TESTS_DELAY)  # let the server "rest"
